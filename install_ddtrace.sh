e71d868a2712d16dc21a926da73195073ee7f445
Santiago M. Mola <santiago.mola@datadoghq.com>
2022-10-07 14:20:40 +0200

    Update to latest library from master (#5)

--- install_ddtrace.sh
+++ install_ddtrace.sh
@@ -5,7 +5,7 @@ set -eu
 mkdir /dd-tracer
 
 if [ $(ls /binaries/dd-java-agent*.jar | wc -l) = 0 ]; then
-    BUILD_URL="https://output.circle-artifacts.com/output/job/bbd728fc-515d-4c4b-a778-8fa331e50721/artifacts/0/libs/dd-java-agent-0.111.0-SNAPSHOT.jar"
+    BUILD_URL="https://output.circle-artifacts.com/output/job/c09522b4-9e33-4c6f-b582-c2dcd40b48e0/artifacts/0/libs/dd-java-agent-0.111.0-SNAPSHOT.jar"
     echo "install from Github release: $BUILD_URL"
     curl  -Lf -o /dd-tracer/dd-java-agent.jar $BUILD_URL
 
